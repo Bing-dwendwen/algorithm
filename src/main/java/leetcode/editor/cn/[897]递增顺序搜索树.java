efb780c80dcd6c00618a5a860a770b3f9fc3a8cc
Bing-dwendwen <bing_dwendwen@163.com>
2022-06-29 15:35:47 +0800

    112，897

---
+++ src/main/java/leetcode/editor/cn/[897]递增顺序搜索树.java
@@ -0,0 +1,100 @@
+//给你一棵二叉搜索树的 root ，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右
+//子节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+//输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [5,1,7]
+//输出：[1,null,5,null,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的取值范围是 [1, 100] 
+// 0 <= Node.val <= 1000 
+// 
+// Related Topics 栈 树 深度优先搜索 二叉搜索树 二叉树 👍 288 👎 0
+
+
+package leetcode.editor.cn;
+
+//递增顺序搜索树
+
+import java.util.LinkedList;
+import java.util.List;
+
+class P897IncreasingOrderSearchTree{
+     public static void main(String[] args) {
+         //测试代码
+        Solution solution = new P897IncreasingOrderSearchTree().new Solution();
+
+     }
+    //力扣代码
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        inorder(root,res);
+        TreeNode dummyNode = new TreeNode(-1);
+        TreeNode currNode = dummyNode;
+        for (int value :
+                res) {
+            currNode.right = new TreeNode(value);
+            currNode = currNode.right;
+        }
+        return dummyNode.right;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res){
+        if (root == null){
+            return;
+        }
+
+        inorder(root.left,res);
+        res.add(root.val);
+        inorder(root.right,res);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
